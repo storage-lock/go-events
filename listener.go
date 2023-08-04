ff4a06910ce989d94f9f00de6903d77118994631
CC11001100 <CC11001100@qq.com>
2023-08-05 00:43:07 +0800

    feat: listener add name method

--- listener.go
+++ listener.go
@@ -6,21 +6,32 @@ import "context"
 
 // Listener 事件监听器，当有任何事件的时候都会触发此监听器把事件传给它处理
 type Listener interface {
+
+	// Name 当有多个事件监听器的时候用于区分彼此
+	Name() string
+
+	// On 每次事件被触发的时候此函数被执行一次
 	On(ctx context.Context, e *Event)
 }
 
 // ------------------------------------------------- --------------------------------------------------------------------
 
 type ListenerWrapper struct {
+	name         string
 	listenerFunc func(ctx context.Context, e *Event)
 }
 
 var _ Listener = &ListenerWrapper{}
 
-func NewListenerWrapper(listenerFunc func(ctx context.Context, e *Event)) *ListenerWrapper {
+func NewListenerWrapper(name string, listenerFunc func(ctx context.Context, e *Event)) *ListenerWrapper {
 	return &ListenerWrapper{
+		name:         name,
 		listenerFunc: listenerFunc,
 	}
+}
+
+func (x *ListenerWrapper) Name() string {
+	return x.name
 }
 
 func (x *ListenerWrapper) On(ctx context.Context, e *Event) {
