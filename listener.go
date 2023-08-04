4168494ed47a81ffa33449bb9d3c4b6db1391112
CC11001100 <CC11001100@qq.com>
2023-08-04 23:53:38 +0800

    feat: add ListenerWrapper

--- listener.go
+++ listener.go
@@ -2,7 +2,29 @@ package events
 
 import "context"
 
+// ------------------------------------------------- --------------------------------------------------------------------
+
 // Listener 事件监听器，当有任何事件的时候都会触发此监听器把事件传给它处理
 type Listener interface {
 	On(ctx context.Context, e *Event)
 }
+
+// ------------------------------------------------- --------------------------------------------------------------------
+
+type ListenerWrapper struct {
+	listenerFunc func(ctx context.Context, e *Event)
+}
+
+var _ Listener = &ListenerWrapper{}
+
+func NewListenerWrapper(listenerFunc func(ctx context.Context, e *Event)) *ListenerWrapper {
+	return &ListenerWrapper{
+		listenerFunc: listenerFunc,
+	}
+}
+
+func (x *ListenerWrapper) On(ctx context.Context, e *Event) {
+	x.listenerFunc(ctx, e)
+}
+
+// ------------------------------------------------- --------------------------------------------------------------------
